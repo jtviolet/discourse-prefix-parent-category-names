0e40cec0d9edb74eea2c4e25f5aa469e57df3c2e
Jordan Violet <https://github.com/jtviolet/discourse-prefix-parent-category-names>
2025-04-01 15:36:36 -0400

    fixing double prefix

--- javascripts/discourse/initializers/ideas-portal.js
+++ javascripts/discourse/initializers/ideas-portal.js
@@ -99,7 +99,7 @@ export default apiInitializer("0.11.1", (api) => {
   // Watch for DOM changes to update sidebar category names
   api.onAppEvent("page:changed", () => {
     // Use a short delay to ensure sidebar is fully rendered
-    updateSidebarCategoryNames();
+    //updateSidebarCategoryNames();
   });
   
   // Run once on initialization
